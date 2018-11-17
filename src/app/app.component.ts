9d6bede73b6b6dcc3679262d74bf83b40958c92f
Guillaume Mesnard <guillaume.mesnard@gmail.com>
2018-11-17 16:44:15 +0100

    Upload project to github

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'magiclicorne';
+  title = 'PWA magiclicorne';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { MatToolbarModule, MatCardModule } from '@angular/material';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ImgCardComponent } from './img-card/img-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImgCardComponent
   ],
   imports: [
     BrowserModule,
+    MatToolbarModule,
+    MatCardModule,
     AppRoutingModule
   ],
   providers: [],
